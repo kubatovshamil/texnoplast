8c5da5bb290bd82ed9375dcafed3e958b8fbbb3a
kubatovshamil <shamil.kubatov@mail.ru>
2021-10-26 15:00:00 +0300

    Сверстал страницы Добавленяи Категорий и Товаров

--- resources/views/admin/layout.blade.php
+++ resources/views/admin/layout.blade.php
@@ -7,6 +7,7 @@
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
     <link rel="stylesheet" href="{{ asset('styles/plugins/fontawesome-free/css/all.min.css')}}">
     <link rel="stylesheet" href="{{ asset('styles/dist/css/adminlte.min.css')}}">
+    <link rel="stylesheet" href="{{ asset('styles/dist/css/style.css')}}">
 </head>
 <body class="hold-transition sidebar-mini">
 <div class="wrapper">
--- resources/views/admin/pages/product/add-product.blade.php
+++ resources/views/admin/pages/product/add-product.blade.php
@@ -19,46 +19,89 @@
                 </div>
             </div>
         </div>
-
         <div class="content">
             <div class="container-fluid">
                 <div class="row">
-                    <form>
-                        <div class="card-body">
-                            <div class="form-group">
-                                <label for="exampleInputEmail1">Поле1 </label>
-                                <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter email">
-                            </div>
-                            <div class="form-group">
-                                <label for="exampleInputPassword1">Поле2</label>
-                                <input type="text" class="form-control" id="exampleInputPassword1" placeholder="Password">
-                            </div>
-                            <div class="form-group">
-                                <label for="exampleInputFile">File input</label>
-                                <div class="input-group">
-                                    <div class="custom-file">
-                                        <input type="file" class="custom-file-input" id="exampleInputFile">
-                                        <label class="custom-file-label" for="exampleInputFile">Choose file</label>
-                                    </div>
-                                    <div class="input-group-append">
-                                        <span class="input-group-text">Upload</span>
+                    <div class="col-md-6">
+                    <div class="card card-primary">
+                        <form>
+                            <div class="card-body">
+                                <div class="form-group">
+                                    <label for="exampleInput1">Наименования категории </label>
+                                    <select class="form-control" id="sel1">
+                                        <option>Поликорбонат сотовый</option>
+                                        <option>Пластиковая тара</option>
+                                        <option>Пластиковые паллеты</option>
+                                        <option>Мусорные контейнеры</option>
+                                        <option>Деревянные поддоны</option>
+                                    </select>
+                                </div>
+
+                                <div class="form-group">
+                                    <label for="exampleInput1">Наименование товара</label>
+                                    <input type="text" class="form-control" id="exampleInput1" placeholder="Введите название товара">
+                                </div>
+
+                                <div class="form-group">
+                                    <label for="exampleInput1">Цена товара</label>
+                                    <input type="number" class="form-control" id="exampleInput1" placeholder="Введите цену товара">
+                                </div>
+
+                                <div class="form-group">
+                                    <label for="exampleInput1">Описание товара</label>
+                                    <input type="text" class="form-control" id="exampleInput1" placeholder="Введите описание товара">
+                                </div>
+
+                                
+                                <div class="form-group">
+                                    <label for="exampleInput1">Скидка товара</label>
+                                    <input type="number" class="form-control" id="exampleInput1" placeholder="Введите скидку товара">
+                                </div>
+
+                                <div class="form-group">
+                                    <label for="exampleInputFile">Изборажения товара</label>
+                                     <div class="input-group">
+                                        <div class="custom-file">
+                                            <input type="file" class="custom-file-input" id="exampleInputFile">
+                                            <label class="custom-file-label" for="exampleInputFile">Выберите файл</label>
+                                        </div>
+                                        <div class="input-group-append">
+                                            <span class="input-group-text">Загрузить</span>
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
-                            <div class="form-check">
-                                <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                                <label class="form-check-label" for="exampleCheck1">Check me out</label>
-                            </div>
-                        </div>
-                        <!-- /.card-body -->
 
-                        <div class="card-footer">
-                            <button type="submit" class="btn btn-primary">Submit</button>
-                        </div>
-                    </form>
+                                <div class="form-group">
+                                    <label for="exampleInput1">Ссылка товара</label>
+                                    <input type="text" class="form-control" id="exampleInput1" placeholder="Введите ссылку товара">
+                                </div>
+
+                                <div class="form-group">
+                                    <label for="exampleInput1">Описание </label>
+                                    <input type="text" class="form-control" id="exampleInput1" placeholder="Введите описание">
+                                </div>
+
+                                <div class="form-group">
+                                    <label for="exampleInput1">Ключевые слова </label>
+                                    <input type="text" class="form-control" id="exampleInput1" placeholder="Введите ключевые слова">
+                                </div>
+
+                                <div class="form-group">
+                                    <label for="exampleInput1">Хит продаж</label>
+                                    <input type="text" class="form-control" id="exampleInput1" placeholder="Введите хит товара">
+                                </div>
+
+                            </div>
+
+                            <div class="card-footer">
+                                <button type="submit" class="btn btn-primary">Добавить</button>
+                            </div>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
+</div>
 
 @endsection
