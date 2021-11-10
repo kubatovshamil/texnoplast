191b076ee64d96fe234430a7341a96211ad5642f
kubatovshamil <shamil.kubatov@mail.ru>
2021-11-10 12:38:14 +0300

    Сохранить

--- app/Http/Services/ProductService.php
+++ app/Http/Services/ProductService.php
@@ -23,41 +23,70 @@ class ProductService
 
     public function updateAttributes($request, $id)
     {
-        $this->getAttributes($request);
+        $attributes = $this->getAttributes($request);
+
+        foreach ($attributes as $k => $attribute) {
+            if(isset($attribute['attr_id'])){
+                if(isset($attribute['attr_name'])){
+                    AttributeValue::where('id', $attribute['attr_id'])
+                        ->update([
+                            'attr_id' => $attribute['attr_name'],
+                            'value' => $attribute['attr_val'],
+                            'product_id' => $id
+                        ]);
+                }else{
+                    AttributeValue::destroy($attribute['attr_id']);
+                }
+            }else{
+
+                if(is_numeric($attribute['attr_name'])){
+                    AttributeValue::create([
+                        'attr_id' => $attribute['attr_name'],
+                        'value' => $attribute['attr_val'],
+                        'product_id' => $id,
+                    ]);
+                }else{
+                    $name = AttributeName::create([
+                        'name' => $attribute['attr_name']
+                    ]);
+                    AttributeValue::create([
+                        'attr_id' => $name->id,
+                        'value' => $attribute['attr_val'],
+                        'product_id' => $id,
+                    ]);
+                }
+            }
+        }
     }
+
 
     public function getAttributes($request){
 
         $attributes = $request->only('attr_name', 'attr_val', 'attr_id');
 
-         ddd($this->compactArray($attributes));
-    }
+        $names = array_keys($attributes);
+        $data = [];
+        array_walk($attributes, function ($item, $key) use ($attributes, &$data, $names){
 
-    public function compactArray($attributes)
-    {
-       $names = array_keys($attributes);
-       $data = [];
+            array_walk($item, function ($value, $k) use ($attributes, $names, &$data, $key){
 
-       array_walk($attributes, function ($item, $key) use ($attributes, &$data, $names){
+                if($attributes[$key]  == $attributes[$names[0]]){
+                    if(is_numeric($value)){
+                        $data[$k][$key] =  (int) $value;
+                    }else{
+                        $data[$k][$key] = $value;
+                    }
+                } elseif ($attributes[$key] == $attributes[$names[1]]){
+                    $data[$k][$key]  = $value;
+                }elseif ($attributes[$key] == $attributes[$names[2]]){
+                    $data[$k][$key] = (int) $value;
+                }
 
-           array_walk($item, function ($value, $k) use ($attributes, $names, &$data, $key){
+            });
 
-               if($attributes[$key]  == $attributes[$names[0]]){
-                   if(is_numeric($value)){
-                       $data[$k][$key] =  (int) $value;
-                   }else{
-                       $data[$k][$key] = $value;
-                   }
-               } elseif ($attributes[$key] == $attributes[$names[1]]){
-                   $data[$k][$key]  = $value;
-               }elseif ($attributes[$key] == $attributes[$names[2]]){
-                   $data[$k][$key] = (int) $value;
-               }
+        });
 
-           });
-
-       });
-       return collect($data);
+        return collect($data);
     }
 
 
