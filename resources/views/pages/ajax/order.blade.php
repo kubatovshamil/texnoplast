f0a48a290750d7716d7a53aae969074a0bd2af79
kubatovshamil <shamil.kubatov@mail.ru>
2021-11-22 11:09:00 +0300

    Styles individual modal form

--- resources/views/pages/ajax/order.blade.php
+++ resources/views/pages/ajax/order.blade.php
@@ -1,23 +1,25 @@
-<div class="box-modal" id="Modal">
+<div class="box-modal individual" id="Modal">
 
     <div class="box-modal_close arcticmodal-close">&#10006;</div>
-
-    <form action="#">
+    <h1 class="individual__title">Индивидуальный заказ</h1>
+    <form action="#" class="individual-order">
         <div class="form-group">
-            <input type="text">
+            <input type="text" class="individual-order__inp" name="surname" placeholder="Фамилия">
         </div>
 
         <div class="form-group">
-            <input type="text">
+            <input type="text" class="individual-order__inp" name="name" placeholder="Имя">
         </div>
 
         <div class="form-group">
-            <input type="text">
+            <input type="text" class="individual-order__inp" placeholder="+7 (989) - 874 - 96 - 64">
         </div>
 
         <div class="form-group">
-            <input type="text">
+            <textarea class="individual-order__inp" name="note" id="" cols="40" rows="30" placeholder="Описание"></textarea>
         </div>
+
+        <button class="individual-order__btn">Отправить</button>
 
     </form>
 
